c4167bb4e88e10024e5ea9781623b15d0135819a
Merouane Atig <merwan@patem.net>
2026-01-02 14:09:59 +0100

    Move player

--- src/game.rs
+++ src/game.rs
@@ -6,12 +6,20 @@ pub fn game_plugin(app: &mut App) {
     app.add_systems(OnEnter(GameState::Game), display_level)
         .add_systems(
             FixedUpdate,
-            control_player.run_if(in_state(GameState::Game)),
+            (
+                control_player.run_if(in_state(GameState::Game)),
+                move_player.run_if(in_state(GameState::Game)),
+            ),
         );
 }
 
 #[derive(Component)]
 struct Player;
+
+#[derive(Component)]
+struct PlayerVelocity {
+    value: f32,
+}
 
 #[derive(Component)]
 struct Asteroid;
@@ -20,6 +28,7 @@ fn display_level(mut commands: Commands, game_assets: Res<GameAssets>) {
     commands.spawn((
         Sprite::from_image(game_assets.player_ship.clone()),
         Player,
+        PlayerVelocity { value: 0.0 },
         StateScoped(GameState::Game),
         children![(
             Sprite::from_image(game_assets.player_engine.clone()),
@@ -38,11 +47,11 @@ fn display_level(mut commands: Commands, game_assets: Res<GameAssets>) {
 
 fn control_player(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    player: Single<(&mut Transform, &Children), With<Player>>,
+    player: Single<(&mut PlayerVelocity, &mut Transform, &Children), With<Player>>,
     mut visibility: Query<&mut Visibility>,
     time: Res<Time>,
 ) -> Result {
-    let (mut player_transform, children) = player.into_inner();
+    let (mut player_velocity, mut player_transform, children) = player.into_inner();
 
     let fixed_rotation_rate = 0.2;
     let rotation_rate = fixed_rotation_rate * 60.0 * time.delta_secs();
@@ -54,13 +63,21 @@ fn control_player(
         player_transform.rotate_z(-rotation_rate);
     }
     if keyboard_input.pressed(KeyCode::KeyW) {
+        player_velocity.value += 0.2;
         visibility
             .get_mut(children[0])?
             .set_if_neq(Visibility::Visible);
     } else {
+        // player_velocity.value = 0.0;
         visibility
             .get_mut(children[0])?
             .set_if_neq(Visibility::Hidden);
     }
+    Ok(())
+}
+fn move_player(player: Single<(&PlayerVelocity, &mut Transform), With<Player>>) -> Result {
+    let (player_velocity, mut player_transform) = player.into_inner();
+    let forward = player_transform.local_y();
+    player_transform.translation += forward * player_velocity.value;
     Ok(())
 }
